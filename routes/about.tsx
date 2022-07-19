74845af3d6104f12578320145cac656369dd8ab5
Tarun Kumar <tarun.kumar@tajir-app.com>
2022-07-20 00:38:16 +0500

    add about

---
+++ routes/about.tsx
@@ -0,0 +1,13 @@
+// routes/about.tsx
+
+/** @jsx h */
+import { h } from "preact";
+
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </main>
+    );
+}
