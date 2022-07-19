185da076faa0e17287ce5778f4564a5adfb12114
Tarun Kumar <tarun.kumar@tajir-app.com>
2022-07-20 04:58:44 +0500

    test handlers

--- routes/about.tsx
+++ routes/about.tsx
@@ -2,7 +2,15 @@
 
 /** @jsx h */
 import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
 
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Foo");
+    return resp;
+  },
+};
 export default function AboutPage() {
   return (
     <main>
